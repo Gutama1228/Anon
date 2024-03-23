3f4229db1dfd764cbb294cbd78e1a70150d0c1ee
gutama <142733438+Gutama1228@users.noreply.github.com>
2024-03-23 07:54:58 +0700

    update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", "") # Dapatkan di @botfather
+TOKEN = os.environ.get("TOKEN", "7054994035:AAElhwuoRYoH9Zxb0oN6zrl1uVjsA9PxU1A") # Dapatkan di @botfather
 OWNER = os.environ.get("OWNER", "kiytam") # jangan dirubah agar tidak error
 GROUP = os.environ.get("GROUP", "") # 
 CHANNEL = os.environ.get("CHANNEL", "gbtktl") # 
